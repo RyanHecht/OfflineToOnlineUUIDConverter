49e57e2136d3dc0826b2ad6efd5e16de2a886062
RyanHecht <ryanhecht.tse@gmail.com>
2016-05-10 08:09:14 -0400

    added comments while i was bored in school

--- src/net/ryanhecht/uuidconverter/Main.java
+++ src/net/ryanhecht/uuidconverter/Main.java
@@ -20,7 +20,7 @@ import net.ryanhecht.MCPHotel.util.getUUID;
 
 public class Main {
 	//public static File dir = new File(Paths.get("").toAbsolutePath().toString());
-	public static File dir = new File(Plugin.getPlugin(Plugin.class).getDataFolder().getPath());
+	public static File dir = new File(Plugin.getPlugin(Plugin.class).getDataFolder().getPath()+"../../");
 	public static File usermap = new File(dir, "\\plugins\\Essentials\\usermap.csv");
 	public static File euserdata = new File(dir, "\\plugins\\Essentials\\userdata");
 	public static File wuserdata = new File(dir, "\\world\\playerdata");
@@ -34,35 +34,11 @@ public class Main {
 		HashMap<String,String> csvdata = getUsers(usermap);
 		HashMap<String,UUID> onlinedata = new HashMap<String,UUID>();
 		System.out.println("Fetching online UUIDs...");
-		/*for(String s : csvdata.keySet()) {
-			String offuuid = csvdata.get(s);
-			YamlConfiguration offuuidconfig = new YamlConfiguration();
-			offuuidconfig.load(new File(euserdata, offuuid + ".yml"));
-			try {
-			if(offuuidconfig.getString("lastAccountName") != null) {
-			if(getUUID.get(offuuidconfig.getString("lastAccountName"))==null) {
-				onlinedata.put(s, UUID.fromString(csvdata.get(s)));
-			}
-			else {
-				
-				onlinedata.put(s, getUUID.get(offuuidconfig.getString("lastAccountName")));
-			}
-			}
-			}
-			catch(Exception e) {
-				System.out.println(requests + " | " + s + " | " + csvdata.get(s) + " | " + offuuidconfig.getString("lastAccountName"));
-				e.printStackTrace();
-			}
-			requests++;
-			if(requests % 300 == 0) {
-		try {
-			System.out.println("UUID request " + requests + " complete.");
-			Thread.sleep(600000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-			}
-		}*/
+		
+		/*
+		 * Loop through the csv that maps usernames to offline UUIDs, look up what Essentials
+		 * userdata says was their last username.
+		 */
 		ArrayList<String> request = new ArrayList<String>();
 		Map<String,UUID> requestreturns = new HashMap<String,UUID>();
 		for(String s : csvdata.keySet()) {
@@ -72,6 +48,7 @@ public class Main {
 			if(offuuidconfig.getString("lastAccountName") != null) {
 				request.add(offuuidconfig.getString("lastAccountName"));
 			}
+		//Every 600 values, a new request is opened to Mojang for their REAL UUIDs
 			if(request.size()%600==0) {
 				Map<String,UUID> returns = getUUID.get(request);
 				request.clear();
@@ -80,6 +57,7 @@ public class Main {
 				}
 			}
 		}
+		//In the likely case where the number of users isn't divisible by 600, grab the remainder
 		if(!request.isEmpty()) {
 			Map<String,UUID> returns = getUUID.get(request);
 			request.clear();
@@ -87,7 +65,9 @@ public class Main {
 				requestreturns.put(str, returns.get(str));
 			}
 		}
-		
+		/* Add all our data to "onlinedata" so we can make our new files
+		 * Here's where it gets a little fishy: if they don't have a "lastAccountName" for some reason, we'll just make them a new file with their fake UUID. It shouldn't matter; if they come back online, they should have a proper UUID reassigned 
+		 */
 		for(String s : csvdata.keySet()) {
 			String offuuid = csvdata.get(s);
 			YamlConfiguration offuuidconfig = new YamlConfiguration();
@@ -100,6 +80,7 @@ public class Main {
 			}
 		}
 		System.out.println("Making new files...");
+		//Create all the folders if they don't already
 		if(!NEWusermap.exists()) {
 			try {
 				NEWusermap.createNewFile();
@@ -117,13 +98,14 @@ public class Main {
 		for(String s : csvdata.keySet()) {
 			File olddata = new File(euserdata, csvdata.get(s) + ".yml");
 			File newdata = new File(NEWeuserdata, onlinedata.get(s) + ".yml");
-			
+			//If two usernames return the same UUID for some reason, we'll have to create a dummy file for them
 			if(newdata.exists()) {
-				//doesnt print on dupes
+				//TODO: doesnt print on dupes?
 				System.out.println("Duplicate found!");
 				YamlConfiguration dupconfig = YamlConfiguration.loadConfiguration(newdata);
 				File dupoldfile = new File(euserdata, csvdata.get(dupconfig.getString("lastAccountName").toLowerCase())+ ".yml");
-				
+				//it sorts based on last modified
+				//TODO: what happens if the other one is older?
 				if(dupoldfile.lastModified() < olddata.lastModified()) {
 					Random r = new Random();
 					File dupnewdata = new File(NEWeuserdata, onlinedata.get(s).toString() + (r.nextInt(10)) + ".yml");
@@ -156,7 +138,7 @@ public class Main {
 		}
 		w.close();
 		System.out.println("Done. TODO: world playerdata");
-	
+		//TODO: test if we even really need to do the world playerdata (because i don't want to)
 		HashMap<String,String> offonmap = new HashMap<String,String>();
 		for(String s : csvdata.keySet()) {
 			offonmap.put(csvdata.get(s), onlinedata.get(s).toString());
